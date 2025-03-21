015ed41eddb0704acc68a8511a295f6ca6d80105
Axel Cabato <192067612+axelcabato@users.noreply.github.com>
2025-03-21 11:45:54 -0700

    Update __main__.py
    Updated default state of __main__ exclude any code in main function.

--- src/__main__.py
+++ src/__main__.py
@@ -1,14 +1,5 @@
-from preGame_module import create_user
-from classes import User
-import sys
-
-
 def main() -> None:
-    # Create new User object and greet user
-    user: User = create_user()
-    
-    
-    sys.exit("Program end reached. Shutting down..")
+    ...
     
     
 if __name__ == "__main__":
