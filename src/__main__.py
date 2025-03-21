958b313c0e18dc218289febd96cd859a768bce9c
Axel Cabato <192067612+axelcabato@users.noreply.github.com>
2025-03-21 14:24:40 -0700

    Update __main__.py
    Added start_new_game() function which starts a new game for the user upon confirmation.

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,11 @@
+from preGame_module import start_new_game
+import sys
+
 def main() -> None:
-    ...
+    start_new_game()
+        
+        
+    sys.exit("Program end reached. Shutting down..")
     
     
 if __name__ == "__main__":
