936ccf6f7c35c454d74019ae67a14b02bf337ac8
Axel Cabato <192067612+axelcabato@users.noreply.github.com>
2025-03-21 14:25:29 -0700

    Update preGame_module.py
    Deleted greet_user() function. Added start_new_game() which starts a new game for the user upon confirmation.

--- src/preGame_module.py
+++ src/preGame_module.py
@@ -1,11 +1,18 @@
-def greet_user() -> None:
-    """Greets user upon entry into the game after asking for their name."""
+def start_new_game() -> None:
+    """Starts a new game for the user upon confirmation."""
     while True:
         try:
-            username: str = input("What is your name? ").title()
+            start_game: str = input("Start a new game? (y/n) ").lower()
         except Exception:
             print("Something went wrong. Try again.")
             continue
         else:
-            print(f"Welcome to Baldur's Gate 0.0.1, {username}!")
-            break
+            if start_game == "y":
+                print("Starting a new game..")
+                break
+            elif start_game == "n":
+                print("A new game has not started.")
+                continue
+            else:
+                print("Input can only be 'y' or 'n'. Try again.")
+                continue
