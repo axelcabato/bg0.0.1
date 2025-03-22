b297f3204774faa9160b0eb85d8f75fa6bc0c56c
Axel Cabato <192067612+axelcabato@users.noreply.github.com>
2025-03-21 22:15:09 -0700

    Update __main__.py
    Added load_game() function.

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,12 @@
 from player import Player
-from preGame_module import start_new_game, create_character
+from preGame_module import start_new_game, create_character,load_game
 import sys
 
 def main() -> None:
-    start_new_game()
     player: Player = Player()
+    start_new_game()
     create_character(current_player=player)
+    load_game()
         
         
     sys.exit("Program end reached. Shutting down..")
